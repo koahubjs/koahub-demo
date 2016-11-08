5827b1b42bd45b8ee5b407233d6a976d33155fdf
heqing <786699892@qq.com>
2016-11-08 15:16:31 +0800

    update static

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import Koahub from "koahubjs";
 import hbs from "koa-hbs";
 import convert from "koa-convert";
 import co from "co";
+import body from "koa-better-body";
+import serve from "koa-static";
 
 const app = new Koahub();
 const koa = app.getKoa();
+
+koa.use(convert(body()));
+koa.use(convert(serve('./www')));
 
 koa.use(convert(hbs.middleware({
     extname: '.html',
