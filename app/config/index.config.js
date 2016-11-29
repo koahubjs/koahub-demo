d45230bd3043cc96f4574656cd3832284047a194
heqing <786699892@qq.com>
2016-11-29 14:05:48 +0800

    update loader

--- app/config/index.config.js
+++ app/config/index.config.js
@@ -1,4 +1,17 @@
 export default {
     port: 3000,
-    default_module: 'home'
+    default_module: 'home',
+
+    //自动加载配置 such as koahub.utils
+	loader: {
+	    "utils": [{
+	        root: 'runtime/util',
+	        suffix: '.util.js'
+	    }, {
+	        root: 'runtime/addon',
+	        suffix: '.util.js',
+	        prefix: '/',
+	        filter: [/\w*\/util\//]
+	    }]
+	}
 }
