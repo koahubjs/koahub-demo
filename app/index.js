58ec40e4fbecd6004dcff4cb4c9f4c2c60ae469b
heqing <786699892@qq.com>
2016-12-02 16:54:15 +0800

    no message

--- app/index.js
+++ app/index.js
@@ -14,7 +14,8 @@ koa.use(hbs.middleware({
     extname: '.html',
     viewPath: './www',
     layoutsPath: './www',
-    partialsPath: './www'
+    partialsPath: './www',
+    disableCache: true //true: 模版修改立即生效（性能差），false：模版修改重启生效（性能提升10倍）
 }));
 
 helpers({
