1a4eec5f9613a1c4b4ceacb685f23063292ac491
heqing <786699892@qq.com>
2017-03-01 21:29:51 +0800

    更新

--- app/config/default.config.js
+++ app/config/default.config.js
@@ -2,6 +2,14 @@ export default {
     port: 3000,
     default_module: 'home',
 
+    session: {
+    	key: 'koahub.js'
+    },
+
+    static: {
+        dir: 'www'
+    },
+
 	//自动加载配置
     loader: {
         "models": {
